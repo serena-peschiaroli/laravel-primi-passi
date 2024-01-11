68476eab35e1fca23d1a7ca92b922e95a9f61835
serena-peschiaroli <serena.peschiaroli@gmail.com>
2024-01-11 17:02:06 +0100

    Add: basic tasklist from tutorial

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,8 +12,47 @@
         
     </header>
     
+
+    <main>
+        <h1> {{ $body_title }} </h1>
+
+                {{-- mostra i task --}}
+        @if (count($tasks) > 0)
+            <ul>
+                @foreach ($tasks as $task)
+                    <li>{{ $task->name }} 
+                        <!-- cancella -->
+                        <form action="/task/{{ $task->id }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button>cancella</button>
+                        </form>
+                    </li>
+                @endforeach
+            </ul>
+        @else
+            <p>Non ci sono task.</p>
+        @endif
+
+        {{-- Aggiungi un task --}}
+        <form action="/task" method="POST">
+            @csrf
+            <label for="task-name">Da fare:</label>
+            <input type="text" name="name" id="task-name" value="{{ old('name') }}">
+            <button type="submit">Aggiungi</button>
+
+            {{-- mostra gli errori --}}
+            @if ($errors->any())
+                <div>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+        </form>
+
+    </main>
 </body>
-<main>
-    <h1> {{ $body_title }} </h1>
-</main>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,9 +20,49 @@ Route::get('/', function () {
     $header_title = "Welcome";
     $body_title = "Hello World!";
 
+    //ipotetici task
+
+    $task = Task::all();
+
     $data = [
         'header_title' => $header_title,
         'body_title' => $body_title,
+        'tasks' => $task, // dovrebbe aggiungere task a data array
     ];
     return view('home', $data);
 });
+
+/**
+ * da tutorial, aggiungere nuovo task
+ */
+Route::post('/task', function (Request $request) {
+    // validazione input
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    // se la validazione fallisce, indietro al form
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    // creare nuovo task
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+});
+
+/**
+ * Delete An Existing Task
+ */
+Route::delete('/task/{id}', function ($id) {
+    Task::findOrFail($id)->delete();
+
+    return redirect('/');
+});
+
+
